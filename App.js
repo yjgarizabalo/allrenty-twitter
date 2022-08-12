d577c94b1ec33f53e978545dd83c48ead57df2fa
Yair G <yjgarizabalo@gmail.com>
2022-08-12 00:24:08 -0500

    [allrenty] add Hook

--- App.js
+++ App.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from "react"
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default class App extends React.Component {
+const App = () => {
 
-  componentDidMount = () => {
-    fetch('https://twitter154.p.rapidapi.com/user/id?user_id=831684179544985602', {
+  const [data, setInfo] = useState([]);
+
+  const getInfo = () => {
+    const options = {
       method: 'GET',
       headers: {
         'X-RapidAPI-Key': 'a1b63d43c2msha035d86a859d696p18aa37jsnc666f95725de',
         'X-RapidAPI-Host': 'twitter154.p.rapidapi.com'
       }
-    })
-  
-    .then((response) => response.json())
-    .then((responseJson) => {
-      console.log(responseJson);
-    })
+    };
 
-    .catch((error) => {
-      console.error(error)
-    });
+    fetch('https://twitter154.p.rapidapi.com/user/id?user_id=831684179544985602', options)
+      .then(res=> res.json())
+      .then(resJson => {
+        console.log('data', resJson);
+        setInfo(resJson);
+      }).catch(err => {console.log(err)})
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>allrenty</Text>
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+
+
+  return (
+    <View style={styles.container}>
         <StatusBar style="auto" />
-      </View>
-    );
-  }
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item}) => {
+            return (
+              <View>
+                <Text>{item.username}</Text>
+              </View>
+            )
 
-}
+          }}
+        />
+    </View>
+  )
+
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -40,5 +56,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    TextColor: '#232323',
   },
 });
+
+export default App;
